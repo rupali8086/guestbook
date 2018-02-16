6d1f367bc2764515e03828c10ca2aad3561e480e
rupali.meshram <meshram.rupali86@gmail.com>
2018-02-16 10:34:59 -0500

    added files

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,7 +6,7 @@ $(document).ready(function(){
     authDomain: "guestbook-8aaea.firebaseapp.com",
     databaseURL: "https://guestbook-8aaea.firebaseio.com",
     projectId: "guestbook-8aaea",
-    storageBucket: "",
+    storageBucket: "guestbook-8aaea.appspot.com",
     messagingSenderId: "417845094402"
   };
   firebase.initializeApp(config);
@@ -58,48 +58,8 @@ $(document).ready(function(){
   // add user data
   $("#addData").on("click", function(event){
     event.preventDefault();
-    // validations for users
-  /*$("#login").validate({
-
-    errorClass: 'invalid',
-             errorPlacement: function (error, element) {
-        $(element)
-            .closest("form")
-            .find("label[for='" + element.attr("id") + "']")
-            .attr('data-error', error.text());
-      },
-      // Specify validation rules
-      rules: {
-        firstname: "required",
-        email: {
-          required: true,
-          email: true
-        },
-        phone: {
-           required: true,
-             number: true
-         }
-        
-      },
-      // Specify validation error messages
-      messages: {
-        firstname: "Please enter your firstname",
-        
-        phone: {
-                required: "Please enter your phone number",
-                number: "Please enter only numeric value"
-          },
-        email: "Please enter a valid email address"
-      },
-      // Make sure the form is submitted to the destination defined
-      // in the "action" attribute of the form when valid
-      submitHandler: function(form) {
-        event.preventDefault();
-        // registeruser();
-       }
-    });
-
-*/
+    console.log("hi");
+    
     var date1 = $("#date").val().trim();
     var firstName = $("#firstName").val().trim();
     var lastName = $("#lastName").val().trim();
@@ -113,9 +73,9 @@ $(document).ready(function(){
     var textnotify = $("#textnotify").val();
 
     var userinfo = {
-        date: date,
-        firstname: firstname,
-        lastname: lastname,
+        date1: date1,
+        firstName: firstName,
+        lastName: lastName,
         email1: email1,
         phone1: phone1,
         addres: addres,
@@ -126,6 +86,7 @@ $(document).ready(function(){
         textnotify: textnotify
         }
         database.ref().push(userinfo);
+        console.log(userinfo);
         alert("Angel's  Info successfully added");
         // Prevents moving to new page
         return false;
@@ -134,24 +95,26 @@ $(document).ready(function(){
     });
   // for  entries from user
   database.ref().on("child_added", function(childSnapshot, prevChildKey){
-    var date =  childSnapshot.val().date;
-    var firstName = childSnapshot.val().firstname;
-    var lastName = childSnapshot.val().lastname;
-    var email = childSnapshot.val().email;
-    var ph = childSnapshot.val().phone;
-    var address = childSnapshot.val().address;
-    var city = childSnapshot.val().city;
+    console.log(childSnapshot.val());
+
+    var date =  childSnapshot.val().date1;
+    var firstname = childSnapshot.val().firstName;
+    var lastname = childSnapshot.val().lastName;
+    var email = childSnapshot.val().email1;
+    var ph = childSnapshot.val().phone1;
+    var address = childSnapshot.val().addres;
+    var city = childSnapshot.val().city1;
     var zip = childSnapshot.val().zipcode;
     var aboutUs = childSnapshot.val().about;
     var emailNotify = childSnapshot.val().emailnotify;
     var textNotify = childSnapshot.val().textnotify;
 
     // Add each user data into the table
-  $("#userTable > tbody").prepend("<tr><td>" + date + "</td><td>" + firstName + "</td><td>" + lastName + "</td><td>" + email  + "</td><td>" + ph + "</td><td>"+ address + "</td><td>"+ city +"</td><td>"+ zip +"</td><td>"+ aboutUs +"</td><td>"+ emailNotify + "</td><td>"+ textNotify + "</td></tr>");
+  $("#userTable > tbody").prepend("<tr><td>" + date + "</td><td>" + firstname + "</td><td>" + lastname + "</td><td>" + email  + "</td><td>" + ph + "</td><td>"+ address + "</td><td>"+ city +"</td><td>"+ zip +"</td><td>"+ aboutUs +"</td><td>"+ emailNotify + "</td><td>"+ textNotify + "</td></tr>");
   
   }, function(err) {
         console.log(err);
-    });
+});
 
 
 
